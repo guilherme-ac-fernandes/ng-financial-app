4240b0d0e35617eb814a84f7b026e0007a82cd7a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 12:37:59 -0300

    feat: criação da página de registro/cadastro

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
+import Input from "../components/Input";
+
 export default function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAble, setIsAble] = useState(true);
+
+  const handleSubmit = () => {
+    console.log(username, password);
+  };
+
+  useEffect(() => {
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if(username.length > 3 && password.match(regexPassword)) {
+      return setIsAble(false);
+    }
+    setIsAble(true);
+  }, [username, password, setIsAble]);
+
   return (
-    <main>
+    <section>
       <h1>Register</h1>
-    </main>
+      <form>
+        <Input
+          id={ 'register-username' }
+          label={ 'Nome do usuário:' }
+          type={ 'text' }
+          value={ username }
+          setValue={ setUsername }
+          dataTestId={ 'register-username' }
+          placeholder={ 'username' }
+        />
+        <Input
+          id={ 'register-password' }
+          label={ 'Senha do usuário:' }
+          type={ "password" }
+          value={ password }
+          setValue={ setPassword }
+          dataTestId={ 'register-password' }
+          placeholder={ '********' }
+        />
+        <Button
+          text={ 'Register' }
+          type={ "button" }
+          disabled={ isAble }
+          handleSubmit={ handleSubmit }
+          dataTestId={ 'register-button' }
+        />
+      </form>
+    </section>
   );
 }
+
