b280be435d95de27faf0636f598927799c3ad780
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 13:16:53 -0300

    feat: criação de um seed de contas default (teste)

---
+++ backend/src/database/seeders/01-account.js
@@ -0,0 +1,16 @@
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert('Accounts', [
+      { balance: 10000 },
+      { balance: 10000 },
+      { balance: 10000 },
+      { balance: 10000 },
+      { balance: 10000 },
+      { balance: 10000 },
+    ], {});
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('Accounts', null, {});
+  },
+};
