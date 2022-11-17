edc883c41446766c33afef4d6c23c2c41cb4bf31
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 19:57:51 -0300

    feat: criação inicial do model das contas

---
+++ backend/src/models/AccountModel.ts
@@ -0,0 +1,18 @@
+import { IAccount, ICreateAccount } from '../interfaces/IAccount';
+import Account from '../database/models/Account';
+import { Transaction } from 'sequelize/types';
+
+export default class AccountModel {
+  protected _model = Account;
+
+  async findOne(id: number): Promise<ICreateAccount | null> {
+    return this._model.findOne({ where: { id } });
+  }
+
+  async create(
+    { balance }: IAccount,
+    transaction: Transaction
+  ): Promise<ICreateAccount | null> {
+    return this._model.create({ balance }, { transaction });
+  }
+}
