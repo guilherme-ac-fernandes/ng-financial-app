bb701109d83f820f745a7849304e39f076f639b3
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 19:56:53 -0300

    feat: criação das interfaces de User

---
+++ backend/src/interfaces/IUser.ts
@@ -0,0 +1,12 @@
+export interface ILogin {
+  username: string,
+  password: string,
+}
+
+export interface IUser extends ILogin {
+  accountId: number,
+}
+
+export interface ICreateUser extends IUser {
+  id: number,
+}
