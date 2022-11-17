712ebf19f9ddf1fdc1f33c99de0db1481c4b7aad
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 12:37:19 -0300

    feat: criação do componente de input

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,34 @@
+interface InputProps {
+  id: string,
+  label: string,
+  type: 'text' | 'password',
+  value: string,
+  setValue: (value: string) => void,
+  dataTestId: string,
+  placeholder: string,
+}
+
+export default function Input({
+  id,
+  label,
+  type,
+  value,
+  setValue,
+  dataTestId,
+  placeholder,
+}: InputProps) {
+  return (
+    <label htmlFor={ id }>
+      { label }
+      <input
+        id={ id }
+        className='login_input'
+        type={ type }
+        value={ value }
+        onChange={ ({ target }) => setValue(target.value) }
+        data-testid={ dataTestId }
+        placeholder={ placeholder }
+      />
+    </label>
+  );
+}
