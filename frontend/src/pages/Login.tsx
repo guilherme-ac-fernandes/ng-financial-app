b72b93400f2ca0304b97dbdb529ed6687c125646
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 12:37:42 -0300

    feat: criação da página de login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import Input from "../components/Input";
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAble, setIsAble] = useState(true);
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    console.log(username, password);
+  };
+
+  useEffect(() => {
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if(username.length > 3 && password.match(regexPassword)) {
+      return setIsAble(false);
+    }
+    setIsAble(true);
+  }, [username, password, setIsAble]);
+
   return (
-    <main>
+    <section>
       <h1>Login</h1>
-    </main>
+      <form>
+        <Input
+          id={ 'login-username' }
+          label={ 'Nome do usuário:' }
+          type={ 'text' }
+          value={ username }
+          setValue={ setUsername }
+          dataTestId={ 'login-username' }
+          placeholder={ 'username' }
+        />
+        <Input
+          id={ 'login-password' }
+          label={ 'Senha do usuário:' }
+          type={ "password" }
+          value={ password }
+          setValue={ setPassword }
+          dataTestId={ 'login-password' }
+          placeholder={ '********' }
+        />
+        <Button
+          text={ 'Login' }
+          type={ "button" }
+          disabled={ isAble }
+          handleSubmit={ handleSubmit }
+          dataTestId={ 'login-button' }
+        />
+        <Button
+          text={ 'Register' }
+          type={ "button" }
+          disabled={ false }
+          handleSubmit={ () => navigate('/register') }
+          dataTestId={ 'login-button' }
+        />
+      </form>
+    </section>
   );
 }
