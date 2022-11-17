e89626dbe211ebd5263ced88378b20e92bb9f6fe
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 17:47:33 -0300

    feat: criação da migrations das transações

---
+++ backend/src/database/migrations/03-create-transactions.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      debitedAccountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'debited_account_id',
+        foreignKey: true,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      creditedAccountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'credited_account_id',
+        foreignKey: true,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      value: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Transactions');
+  },
+};
