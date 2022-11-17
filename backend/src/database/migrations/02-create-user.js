448cb87651e45d77461677fcc1f80fdacef1707f
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 15:48:27 -0300

    feat: criação da migration de usuário

---
+++ backend/src/database/migrations/02-create-user.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      accountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'account_id',
+        foreignKey: true,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Users');
+  },
+};
