c9d6a6b39763f24468b5277f73b31e5df7b94b3c
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 22:28:57 -0300

    feat: implementa o controller de findAll e login

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -8,13 +8,24 @@ export default class CarController {
     this._user = service;
   }
 
+  public async findAll(_req: Request, res: Response, next: NextFunction) {
+    const { code, message, data } = await this._user.findAll();
+    if (message) return next({ code, message });
+    return res.status(code).json(data);
+  }
+
+  public async login(req: Request, res: Response, next: NextFunction) {
+    const { username, password } = req.body;
+    const { code, message, data } = await this._user.login({ username, password });
+    if (message) return next({ code, message });
+    return res.status(code).json(data);
+  }
+
   public async create(req: Request, res: Response, next: NextFunction) {
     const { username, password } = req.body;
-    const result = await this._user.create({ username, password });
-    if (result.message) {
-      next({ code: 500, message: result.message });
-    }
-    return res.status(201).json(result);
+    const { code, message, data } = await this._user.create({ username, password });
+    if (message) return next({ code, message });
+    return res.status(code).json(data);
   }
 
 }
