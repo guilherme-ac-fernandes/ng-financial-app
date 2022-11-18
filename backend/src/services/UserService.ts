451992e62535e91abbd39107621e5e6e0eca992a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-17 21:30:18 -0300

    feat: implementação da função create no service de usuário

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,33 +1,52 @@
-import { ICreateUser, ILogin } from '../interfaces/IUser';
+import { ILogin } from '../interfaces/IUser';
 import UserModel from '../models/UserModel';
-// import sequelize from '../database/models';
+import AccountModel from '../models/AccountModel';
+import Sequelize from '../database/models';
+import BcryptService from '../helpers/BcriptHelpers';
+import TokenHelpers from '../helpers/TokenHelpers';
 
 export default class UserService {
-  private _model: UserModel;
+  private _user: UserModel;
+  private _account: AccountModel;
 
-  constructor(model: UserModel) {
-    this._model = model;
+  constructor(userModel: UserModel, accountModel: AccountModel) {
+    this._user = userModel;
+    this._account = accountModel;
   }
 
   public async findOneUsername(username: string) {
-    const userFound = this._model.findOne(username);
+    const userFound = this._user.findOne(username);
     if (!userFound) return null;
     return userFound;
   }
 
-  public async login(userLogin: ILogin) {
-    const userFound = this.findOneUsername(
-      userLogin.username
-    ) as unknown as ICreateUser;
-    if (userFound.password !== userLogin.password) {
-      return { code: 401, message: 'Invalid password' };
+  // public async login(userLogin: ILogin) {
+  //   const userFound = this.findOneUsername(
+  //     userLogin.username
+  //   ) as unknown as ICreateUser;
+  //   if (userFound.password !== userLogin.password) {
+  //     return { code: 401, message: 'Invalid password' };
+  //   }
+  // }
+
+  public async create({ username, password }: ILogin) {
+    const transaction = await Sequelize.transaction();
+    try {
+      const { id: accountId } = await this._account.create(
+        { balance: 10000 },
+        transaction
+      );
+      await this._user.create(
+        { username, password: BcryptService.encrypt(password), accountId },
+        transaction
+      );
+      await transaction.commit();
+      const token = TokenHelpers.createToken(username);
+      return { token, username };
+    } catch (error) {
+      console.log(error);
+      await transaction.rollback();
+      return { message: 'Internal server error' };
     }
   }
-
-  // public async create() {
-  //   const t = await sequelize.transaction();
-  //   try {
-  //     const { id } = this.create;
-  //   } catch (error) {}
-  // }
 }
