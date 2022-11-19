65c2428066cd0a4ea342c43b9df2b5612cd99211
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-19 01:21:50 -0300

    feat: criação do middleware de validação das transações

---
+++ backend/src/middlewares/TransactionValidations.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+import { ITransaction } from '../interfaces/ITransaction';
+
+export default async (req: Request, _res: Response, next: NextFunction) => {
+  const { debitedAccountId, creditedAccountId, value } = req.body as ITransaction;
+  if (value <= 0) {
+    return next({ code: 400, message: 'Transaction can\'t equal or under zero' });
+  }
+  if (debitedAccountId === creditedAccountId) {
+    return next({ code: 401, message: 'Transaction must be for different users' });
+  }
+  next();
+}
