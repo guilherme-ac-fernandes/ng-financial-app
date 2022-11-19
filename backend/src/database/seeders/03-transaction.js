2359ad88ce5d567ac9915c43c3d705371112e266
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-19 11:39:29 -0300

    refactor: adiciona horários diferentes

--- backend/src/database/seeders/03-transaction.js
+++ backend/src/database/seeders/03-transaction.js
@@ -5,13 +5,13 @@ module.exports = {
         debitedAccountId: 1,
         creditedAccountId: 2,
         value: 28,
-        createdAt: new Date(),
+        createdAt: new Date("2022-11-19T14:15:13.000Z"),
       },
       {
         debitedAccountId: 2,
         creditedAccountId: 1,
         value: 16,
-        createdAt: new Date(),
+        createdAt: new Date("2022-11-19T19:15:13.000Z"),
       },
     ], {});
   },
