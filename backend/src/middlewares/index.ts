e3a00c2882548f1c424043d1980234686a924333
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-19 01:22:43 -0300

    feat: implementação da validação na rota de criação de uma transação

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,5 +1,11 @@
-import error from './error';
+import TransactionValidations from './TransactionValidations';
 import UserValidations from './UserValidations';
+import error from './error';
 import auth from './auth';
 
-export default { error, UserValidations, auth };
+export default {
+  TransactionValidations,
+  UserValidations,
+  error,
+  auth,
+};
--- backend/src/routes/TransactionRoute.ts
+++ backend/src/routes/TransactionRoute.ts
@@ -12,8 +12,11 @@ const transactionModel = new TransactionModel();
 const transactionService = new TransactionService(transactionModel, accountModel);
 const transactionController = new TransactionController(transactionService);
 
-route.post('/', Middlewares.auth, (req, res, next) =>
-  transactionController.create(req, res, next)
+route.post(
+  '/',
+  Middlewares.auth,
+  Middlewares.TransactionValidations,
+  (req, res, next) => transactionController.create(req, res, next)
 );
 
 export default route;
