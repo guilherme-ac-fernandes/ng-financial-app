3fce2a88613abc90d09ee399f227d1719aaf7b77
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-20 14:37:39 -0300

    feat: adiciona filtro na rota do retorno de todas as transações e implementa retorno filtrado por data

--- backend/src/models/TransactionModel.ts
+++ backend/src/models/TransactionModel.ts
@@ -1,12 +1,26 @@
 import { ICreateTransaction, ITransaction } from '../interfaces/ITransaction';
 import Transactions from '../database/models/Transaction';
 import { Transaction } from 'sequelize/types';
+import { Op } from 'sequelize';
 
 export default class TransactionModel {
   protected _model = Transactions;
 
-  async findAll(): Promise<ICreateTransaction[] | null> {
-    return this._model.findAll();
+  async findAll(accountId: number): Promise<ICreateTransaction[] | null> {
+    return this._model.findAll({
+      where: {
+        [Op.or]: [{ debitedAccountId: accountId }, { creditedAccountId: accountId }],
+      }
+    });
+  }
+
+  async findByDate(accountId: number, date: string = ''): Promise<ICreateTransaction[] | null> {
+    return this._model.findAll({
+      where: {
+        [Op.or]: [{ debitedAccountId: accountId }, { creditedAccountId: accountId }],
+        [Op.and]: [{ createdAt: { [Op.gte]: new Date(date) } }],
+      },
+    });
   }
 
   async create(
