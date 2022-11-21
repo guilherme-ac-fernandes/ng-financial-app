6fc3d5e6243c0128bc754e8f091d7427ab359d8b
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-21 13:36:59 -0300

    style: estilização inicial do botão

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,9 +1,12 @@
+import styles from './styles/Button.module.css';
+
 interface ButtonProps {
   text: string;
   type: 'submit' | 'button';
   handleSubmit: () => void;
   disabled: boolean,
   dataTestId: string;
+  buttonClass?: string,
 }
 
 export default function Button({
@@ -12,9 +15,11 @@ export default function Button({
   handleSubmit,
   disabled,
   dataTestId,
+  buttonClass,
 }: ButtonProps) {
   return (
     <button
+      className={`${styles.buttonContainer} ${buttonClass || ''}`}
       type={ type }
       onClick={ handleSubmit }
       disabled={ disabled }
