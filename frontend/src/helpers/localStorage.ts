14188f7974786ab257bf4f7991334ad66436547a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-20 21:05:35 -0300

    refactor: correção da tipagem da função getItem

--- frontend/src/helpers/localStorage.ts
+++ frontend/src/helpers/localStorage.ts
@@ -1,10 +1,10 @@
 import { IUser } from "../interfaces/IUser";
 
-export const setItem = (key: "string", value: IUser) => {
+export const setItem = (key: string, value: IUser) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
-export const getItem = (key: any) => {
+export const getItem = (key: string) => {
   const data = localStorage.getItem(key);
   if (data === null) return null
   const dataParse = JSON.parse(data as string);
