dafe1f2aaa490fb8628fd6c1ec4f81967975cb17
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-21 18:20:54 -0300

    style: estilização inicial do componente de tabela semd dados

--- frontend/src/components/EmptyTable.tsx
+++ frontend/src/components/EmptyTable.tsx
@@ -1,5 +1,9 @@
+import styles from './styles/EmptyTable.module.css';
+
 export default function EmptyTable() {
   return (
-    <h4>Nenhuma transação foi encontrada</h4>
+    <section className={styles.emptyTableContainer}>
+      <h5>Nenhuma transação foi encontrada</h5>
+    </section>
   );
 }
