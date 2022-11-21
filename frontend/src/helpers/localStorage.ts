63c93d58cde820d8b2953ad8651d93d5d29fbc48
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-20 21:00:06 -0300

    feat: criação do arquivo de funções para alterações no localStorage

---
+++ frontend/src/helpers/localStorage.ts
@@ -0,0 +1,12 @@
+import { IUser } from "../interfaces/IUser";
+
+export const setItem = (key: "string", value: IUser) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getItem = (key: any) => {
+  const data = localStorage.getItem(key);
+  if (data === null) return null
+  const dataParse = JSON.parse(data as string);
+  return dataParse;
+};
