6fba8311d8db7d9c84afc6159ac786789ebe4c05
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-21 00:38:30 -0300

    feat: adiciona renderização condicional

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,4 +1,5 @@
 import { ITransactions } from '../interfaces/ITransactions';
+import EmptyTable from './EmptyTable';
 
 interface TableProps {
   transactions: ITransactions[] | [];
@@ -6,27 +7,33 @@ interface TableProps {
 
 export default function Table({ transactions }: TableProps) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Item</th>
-          <th>debitedAccountId</th>
-          <th>creditedAccountId</th>
-          <th>Value</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map((transaction, index) => (
-          <tr key={`table-${index}`}>
-            <td>{index + 1}</td>
-            <td>{transaction.debitedAccountId}</td>
-            <td>{transaction.creditedAccountId}</td>
-            <td>{transaction.value}</td>
-            <td>{transaction.createdAt}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      {transactions.length === 0 ? (
+        <EmptyTable />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>debitedAccountId</th>
+              <th>creditedAccountId</th>
+              <th>Value</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction, index) => (
+              <tr key={`table-${index}`}>
+                <td>{index + 1}</td>
+                <td>{transaction.debitedAccountId}</td>
+                <td>{transaction.creditedAccountId}</td>
+                <td>{transaction.value}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 }
