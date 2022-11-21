61a3d0d9c74d509b14e62ec20dd0746c7bd93b0e
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-20 21:02:47 -0300

    feat: implementa os componentes criados na página do usuário

--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -1,20 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import DisplayUser from "../components/DisplayUser";
+import Filters from "../components/Filters";
 import Header from "../components/Header";
+import Loading from "../components/Loading";
+import Table from "../components/Table";
+import { getBalance, getTransactionsDefault } from "../helpers/api";
+import { getItem } from "../helpers/localStorage";
+import { IAccount } from "../interfaces/IAccount";
+import { ITransactions } from "../interfaces/ITransactions";
+import { IUser } from "../interfaces/IUser";
 
 export default function Transactions() {
+  const [user, setUser] = useState<IUser>({});
+  const [balance, setBalance] = useState<string>('');
+  const [transactions, setTransactions] = useState<ITransactions[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!localStorage.getItem('user')) {
-      return navigate('/');
-    }
+    setLoading(true);
+    const userLocalStorage = getItem('user') as unknown as IUser;
+    if (!userLocalStorage) return navigate('/');
+    setUser(userLocalStorage);
+    axiosRequest();
+    setLoading(false);
   }, [navigate]);
 
+  const axiosRequest = async () => {
+    const balanceAxios = await getBalance() as unknown as IAccount;
+    setBalance(balanceAxios.balance as string);
+    const transactionsAxios = await getTransactionsDefault() as unknown as ITransactions[];
+    setTransactions(transactionsAxios);
+  };
+
   return (
-    <main>
-      <h1>Transactions</h1>
+    <main> 
       <Header />
+      { loading ? (
+        <Loading />
+      ) : (
+        <section>
+          <DisplayUser
+            username={ user.username }
+            balance={ balance }
+          />
+          <p>Modal nova transação</p>
+          <Filters />
+          <Table transactions={ transactions } />
+        </section>
+      ) }
     </main>
   );
 }
