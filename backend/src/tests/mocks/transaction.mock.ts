9722dd57d5aa4d0198ea7cb2d5ee53e6cd871b3e
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-22 03:29:43 -0300

    feat: adiciona antigas transações para teste de filtragem

--- backend/src/tests/mocks/transaction.mock.ts
+++ backend/src/tests/mocks/transaction.mock.ts
@@ -1,7 +1,26 @@
+const OLD_TRANSACTION = [
+  {
+    id: 1,
+    debitedAccountId: 1,
+    creditedAccountId: 2,
+    value: 28,
+    createdAt: "2022-11-19T14:15:13.000Z",
+  },
+  {
+    id: 2,
+    debitedAccountId: 2,
+    creditedAccountId: 1,
+    value: 16,
+    createdAt: "2022-11-19T19:15:13.000Z",
+  },
+]
+
 export const TRANSACTION_CREATED = {
-  createdAt: '2022-11-20T00:18:06.218Z',
-  id: 1,
-  creditedAccountId: 2,
+  id: 3,
   debitedAccountId: 1,
+  creditedAccountId: 2,
   value: 12.00,
+  createdAt: '2022-11-20T00:18:06.218Z',
 };
+
+export const ALL_TRANSACTION = [...OLD_TRANSACTION, TRANSACTION_CREATED];
