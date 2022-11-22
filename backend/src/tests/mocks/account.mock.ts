a3bd134f4784fd9120e39bab5596ae9bf33f7529
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-22 02:56:52 -0300

    feat: atualização dos mocks da conta dos usuários

--- backend/src/tests/mocks/account.mock.ts
+++ backend/src/tests/mocks/account.mock.ts
@@ -1,4 +1,19 @@
-export const ACCOUNT_USER = {
+export const ACCOUNT_USER_1 = {
   id: 1,
-  balance: 82.00,
+  balance: 88.00,
+};
+
+export const ACCOUNT_USER_2 = {
+  id: 1,
+  balance: 112.00,
+};
+
+export const ACCOUNT_USER_1_UPDATED = {
+  id: 1,
+  balance: 76.00,
+};
+
+export const ACCOUNT_USER_2_UPDATED = {
+  id: 1,
+  balance: 124.00,
 };
