6ec64c264184703d1b94837a7305ffade3e6db9d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-22 01:08:55 -0300

    feat: criação do mock do retorno do login

---
+++ backend/src/tests/mocks/user.mock.ts
@@ -0,0 +1,6 @@
+export const REQUEST_LOGIN = {
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJhcm5leXN0aW5zb24iLCJhY2NvdW50SWQiOjEsImlhdCI6MTY2OTA4OTA2MCwiZXhwIjoxNjY5MTc1NDYwfQ.bYsKiBHC-PTfJZhqg_phr5ljFWhNkRSar68RHmymr-4',
+  username: 'barneystinson',
+  accountId: 1,
+};
