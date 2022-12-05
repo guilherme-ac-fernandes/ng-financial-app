d8326e8f910d9e7f05f14794902c688e6f39ed4a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-04 22:17:24 -0300

    refactor: alteração do retorno proveniente da alteração no backend

--- frontend/src/interfaces/ITransactions.ts
+++ frontend/src/interfaces/ITransactions.ts
@@ -1,7 +1,9 @@
 export interface ITransactions {
-  id: number,
-  debitedAccountId: number,
-  creditedAccountId: number,
-  value: string,
-  createdAt: string,
+  id: number;
+  debitedAccountId: number;
+  creditedAccountId: number;
+  value: string;
+  createdAt: string;
+  debitedAccount: { id: number; user: { username: string } };
+  creditedAccount: { id: number; user: { username: string } };
 }
