70d97ceeb512a645622cde2660f4ed5eb69c9849
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-05 13:22:32 -0300

    refaactor: atualização do mock de usuários

--- frontend/src/tests/mocks/users.mock.ts
+++ frontend/src/tests/mocks/users.mock.ts
@@ -1,6 +1,10 @@
-import { IUser } from "../../interfaces/IUser";
-import { userMock, newUserMock } from "./user.mock";
+import { IUser } from '../../interfaces/IUser';
 
 export const usersMock: IUser[] = [
-  userMock, newUserMock
+  { username: 'barneystinson', accountId: 1 },
+  { username: 'lilipad', accountId: 2 },
+  { username: 'tmosby', accountId: 3 },
+  { username: 'robin', accountId: 4 },
+  { username: 'marshall', accountId: 5 },
+  { username: 'themom', accountId: 6 },
 ];
